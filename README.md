de7b7508733dce4e618d3fc14f4d4f65ac182aa5
Sean Lee <xmlee97@gmail.com>
2023-10-18 12:06:23 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-# AnglE
-Angle-Optimized Text Embeddings
+# [AnglE📐: Angle-Optimized Text Embeddings](https://arxiv.org/abs/2309.12871)
+
+> It is Angle 📐, not Angel 👼.
+
+<h2 align='center'>🔥 A New SOTA Model for Semantic Textual Similarity! 
+<a href="https://arxiv.org/abs/2309.12871">
+    <img src="https://img.shields.io/badge/Arxiv-2306.06843-yellow.svg?style=flat-square" alt="https://arxiv.org/abs/2309.12871" />
+</a>
+</h2>
+
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/angle-optimized-text-embeddings/semantic-textual-similarity-on-sick-r-1)](https://paperswithcode.com/sota/semantic-textual-similarity-on-sick-r-1?p=angle-optimized-text-embeddings)
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/angle-optimized-text-embeddings/semantic-textual-similarity-on-sts16)](https://paperswithcode.com/sota/semantic-textual-similarity-on-sts16?p=angle-optimized-text-embeddings)
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/angle-optimized-text-embeddings/semantic-textual-similarity-on-sts15)](https://paperswithcode.com/sota/semantic-textual-similarity-on-sts15?p=angle-optimized-text-embeddings)
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/angle-optimized-text-embeddings/semantic-textual-similarity-on-sts14)](https://paperswithcode.com/sota/semantic-textual-similarity-on-sts14?p=angle-optimized-text-embeddings)
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/angle-optimized-text-embeddings/semantic-textual-similarity-on-sts13)](https://paperswithcode.com/sota/semantic-textual-similarity-on-sts13?p=angle-optimized-text-embeddings)
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/angle-optimized-text-embeddings/semantic-textual-similarity-on-sts12)](https://paperswithcode.com/sota/semantic-textual-similarity-on-sts12?p=angle-optimized-text-embeddings)
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/angle-optimized-text-embeddings/semantic-textual-similarity-on-sts-benchmark)](https://paperswithcode.com/sota/semantic-textual-similarity-on-sts-benchmark?p=angle-optimized-text-embeddings)
+
+
+<details>
+<summary>📊 Click to show main results of AnglE</summary>
+<img src='assets/angle-results.png' width=500>
+</details>
+
+## 🤗 Pretrained Models
+
+| HF | Avg. |
+|----|------|
+| [SeanLee97/angle-llama-7b-nli-20231027](https://huggingface.co/SeanLee97/angle-llama-7b-nli-20231027/tree/main) |   0.8590   |
+
+
+<small>💬 The model above was trained using BERT's hyperparameters. However, Angle-LLaMA is currently actively searching for even better hyperparameters. We plan to release more advanced pre-trained models that will further enhance performance.  Stay tuned ;)😉 </small>
+
+
+**📝 Training Details:**
+
+<details>
+<summary>1) SeanLee97/angle-llama-7b-nli-20231027</summary>
+
+We fine-tuned AnglE-LLaMA using 4 RTX 3090 Ti (24GB), the training script is as follows:
+
+```bash
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 --master_port=1234 train_angle.py \
+--task NLI-STS --save_dir ckpts/NLI-STS-angle-llama-7b \
+--w2 35 --learning_rate 2e-4 --maxlen 45 \
+--lora_r 32 --lora_alpha 32 --lora_dropout 0.1 \
+--save_steps 200 --batch_size 160 --seed 42 --do_eval 0 --load_kbit 4 --gradient_accumulation_steps 4 --epochs 1 
+```
+
+The evaluation script is as follows:
+
+```bash
+CUDA_VISIBLE_DEVICES=0,1 python eval.py \
+    --load_kbit 16 \
+    --model_name_or_path NousResearch/Llama-2-7b-hf \
+    --lora_weight SeanLee97/angle-llama-7b-nli-20231027
+```
+
+</details>
+
+
+## Usage
+
+### Angle-LLaMA
+
+1) using transformers
+
+```python
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel, PeftConfig
+
+peft_model_id = 'SeanLee97/angle-llama-7b-nli-20231027'
+config = PeftConfig.from_pretrained(peft_model_id)
+tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path).bfloat16().cuda()
+model = PeftModel.from_pretrained(model, peft_model_id).cuda()
+
+def decorate_text(text: str):
+    return f'Summarize sentence "{text}" in one word:"'
+
+inputs = 'hello world!'
+tok = tokenizer([decorate_text(inputs)], return_tensors='pt')
+for k, v in tok.items():
+    tok[k] = v.cuda()
+vec = model(output_hidden_states=True, **tok).hidden_states[-1][:, -1].float().detach().cpu().numpy()
+print(vec)
+```
+
+2) using AnglE
+
+Coming soon!
+
+
+### Angle-BERTs
+
+Coming soon!
+
+
+## Train Custom AnglE Model
+
+The training interface is still messy, we are working on making it better. Currently you can modify `train_angle.py` to train your own models.
---
+++ eval.py
@@ -0,0 +1,327 @@
+# -*- coding: utf-8 -*-
+
+# modified from: https://github.com/kongds/Prompt-BERT/blob/main/evaluation.py
+
+import sys
+import os
+import torch
+import logging
+import fcntl
+import time
+import argparse
+from prettytable import PrettyTable
+from transformers import LlamaTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# Set up logger
+logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
+
+# Import SentEval
+sys.path.insert(0, './SentEval')
+import senteval
+
+
+PATH_TO_DATA = './SentEval/data'
+
+
+def print_table(task_names, scores):
+    tb = PrettyTable()
+    tb.field_names = task_names
+    tb.add_row(scores)
+    print(tb)
+
+
+def lock_and_write_file(file_path, content):
+    with open(file_path, 'a') as file:
+        while True:
+            try:
+                # Acquire an exclusive lock (non-blocking)
+                fcntl.flock(file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+                # Perform your write operations here
+                file.write(content + '\n')
+                file.flush()
+
+            except IOError as e:
+                print("File is locked by another process. Can't write.")
+                time.sleep(1)
+            finally:
+                # Release the lock
+                fcntl.flock(file, fcntl.LOCK_UN)
+                break
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prompt', type=str, default='Summarize sentence "{text}" in one word:"')
+    parser.add_argument("--tokenizer_name", type=str, default='')
+    parser.add_argument("--model_name_or_path", type=str,
+                        help="Transformers' model name or path")
+    parser.add_argument("--mode", type=str,
+                        choices=['dev', 'test', 'fasttest'],
+                        default='test',
+                        help="What evaluation mode to use (dev: fast mode, dev results; test: full mode, test results); fasttest: fast mode, test results")
+    parser.add_argument("--task_set", type=str,
+                        choices=['sts', 'transfer', 'full', 'na'],
+                        default='sts',
+                        help="What set of tasks to evaluate on. If not 'na', this will override '--tasks'")
+    parser.add_argument('--load_kbit', type=int,
+                        choices=[4,8,16],
+                        default=8,
+                        help="Load model in kbit")
+
+    parser.add_argument('--avg', action='store_true')
+    parser.add_argument('--lora_weight', type=str, default=None)
+    parser.add_argument('--checkpoint_path', type=str, default=None)
+
+
+    args = parser.parse_args()
+    print('>>> prompt:', args.prompt)
+    if args.load_kbit == 4:
+        from transformers import BitsAndBytesConfig
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model_name_or_path,
+            load_in_4bit=True,
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit=True,
+                llm_int8_threshold=6.0,
+                llm_int8_has_fp16_weight=False,
+                bnb_4bit_compute_dtype=torch.float16,
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_quant_type='nf4',
+            ),
+            torch_dtype=torch.float16,
+            device_map='auto',
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path,
+                                                     device_map='auto',
+                                                     output_hidden_states=True,
+                                                     trust_remote_code=True,
+                                                     load_in_8bit=args.load_kbit == 8,)
+
+    if args.lora_weight is not None:
+        from peft import PeftModel
+        model = PeftModel.from_pretrained(
+            model,
+            args.lora_weight,
+            torch_dtype=torch.float16,
+            device_map={'': 0},
+        )
+        if args.load_kbit == 4:
+            from peft.tuners.lora import LoraLayer
+            for name, module in model.named_modules():
+                if isinstance(module, LoraLayer):
+                    #module = module.to(torch.bfloat16)
+                    module = module.to(torch.float16)
+                if 'norm' in name:
+                    module = module.to(torch.float32)
+                if 'lm_head' in name or 'embed_tokens' in name:
+                    if hasattr(module, 'weight'):
+                        #module = module.to(torch.bfloat16)
+                        if 'opt' in args.model_name_or_path:
+                            module = module.to(torch.float32)
+                        else:
+                            module = module.to(torch.float16)
+
+    if 'llama' in args.model_name_or_path:
+        tokenizer = LlamaTokenizer.from_pretrained(args.model_name_or_path, use_fast=True)
+        tokenizer.bos_token_id = 1
+        tokenizer.eos_token = '</s>'
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+
+    tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+    tokenizer.padding_side = "left"  # Allow batched inference
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Set up the tasks
+    #args.tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']
+    #args.tasks = ['MR']
+    if args.task_set == 'sts':
+        args.tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']
+        if args.mode == 'dev':
+            args.tasks = ['STSBenchmark-dev']
+    elif args.task_set == 'transfer':
+        args.tasks = ['MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC']
+    elif args.task_set == 'full':
+        args.tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']
+        args.tasks += ['MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC']
+
+    # Set params for SentEval
+    if args.mode == 'dev' or args.mode == 'fasttest':
+        # Fast mode
+        params = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 5, 'batch_size': 32}
+        params['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 32,
+                                         'tenacity': 3, 'epoch_size': 2}
+    elif args.mode == 'test':
+        # Full mode
+        params = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 10, 'batch_size':16}
+        params['classifier'] = {'nhid': 0, 'optim': 'adam', 'batch_size': 64,
+                                         'tenacity': 5, 'epoch_size': 4}
+    else:
+        raise NotImplementedError
+
+    # SentEval prepare and batcher
+    def prepare(params, samples):
+        return
+
+    def batcher(params, batch, max_length=None):
+        # Handle rare token encoding issues in the dataset
+        if len(batch) >= 1 and len(batch[0]) >= 1 and isinstance(batch[0][0], bytes):
+            batch = [[word.decode('utf-8') for word in s] for s in batch]
+
+        sentences = [' '.join(s) for s in batch]
+        if max_length == 500:
+            sentences = [tokenizer.decode(tokenizer.encode(s, add_special_tokens=False)[:max_length]) for s in sentences]
+            max_length = 512
+
+        for i, s in enumerate(sentences):
+            if len(s) > 0 and s[-1] not in '.?"\'': s += '.'
+            s = s.replace('"', '\'')
+            if len(s) > 0 and '?' == s[-1]: s = s[:-1] + '.'
+            sentences[i] = args.prompt.format(text=s)
+
+        batch = tokenizer.batch_encode_plus(
+            sentences,
+            return_tensors='pt',
+            padding=True,
+            max_length=max_length,
+            truncation=max_length is not None
+        )
+
+        # Move to the correct device
+        for k in batch:
+            batch[k] = batch[k].to(device) if batch[k] is not None else None
+
+        # Get raw embeddings
+        with torch.no_grad():
+            hidden_states = model(output_hidden_states=True, return_dict=True, **batch).hidden_states
+
+            if args.avg:
+                last_layer = hidden_states[-1]
+                attention_mask = batch['attention_mask'].unsqueeze(-1).expand(last_layer.shape)
+                outputs = (last_layer * attention_mask).mean(1)
+            else:
+                hidden_states = hidden_states[-1]
+                batch_size = hidden_states.shape[0]
+                if model.config.pad_token_id is None and batch_size != 1:
+                    raise ValueError("Cannot handle batch sizes > 1 if no padding token is defined.")
+                if model.config.pad_token_id is None:
+                    sequence_lengths = -1
+                else:
+                    if batch['input_ids'] is not None:
+                        sequence_lengths = (torch.eq(batch['input_ids'], model.config.pad_token_id).long().argmax(-1) - 1).to(
+                            hidden_states.device
+                        )
+                    else:
+                        sequence_lengths = -1
+
+                outputs = hidden_states[torch.arange(batch_size, device=hidden_states.device), sequence_lengths]
+                # outputs = hidden_states[-1][:, -1, :]
+
+            if outputs.dtype == torch.bfloat16:
+                # bfloat16 not support for .numpy()
+                outputs = outputs.float()
+
+            return outputs.cpu()
+
+    results = {}
+    for task in args.tasks:
+        se = senteval.engine.SE(params, batcher, prepare)
+        result = se.eval(task)
+        results[task] = result
+
+    # Print evaluation results
+    if args.mode == 'dev':
+        print("------ %s ------" % (args.mode))
+
+        task_names = []
+        scores = []
+        for task in ['STSBenchmark-dev']:
+            task_names.append(task)
+            if task in results:
+                scores.append("%.2f" % (results[task]['dev']['spearman'][0] * 100))
+            else:
+                scores.append("0.00")
+        print_table(task_names, scores)
+
+        if args.checkpoint_path is not None:
+            # evaluate checkpoints on dev
+            if os.path.exists(os.path.join(args.checkpoint_path, 'dev_results')):
+                max_scores = 0
+                with open(os.path.join(args.checkpoint_path, 'dev_results'), 'r') as f:
+                    for i in f:
+                        max_scores = max(max_scores, float(i.split()[1]))
+            else:
+                max_scores = 0
+
+            # save best checkpoint
+            if float(scores[-1]) >= max_scores:
+                import shutil
+                if args.lora_weight is not None:
+                    shutil.copytree(args.lora_weight, os.path.join(args.checkpoint_path, 'best_model'), dirs_exist_ok=True)
+                else:
+                    shutil.copytree(args.model_name_or_path, os.path.join(args.checkpoint_path, 'best_model'), dirs_exist_ok=True)
+
+            # log dev results
+            with open(os.path.join(args.checkpoint_path, 'dev_results'), 'a') as f:
+                prefix = args.mask_embedding_sentence_template if not args.avg else 'avg'
+                line = prefix + ' ' +str(scores[-1]) + ' ' + \
+                    args.lora_weight if args.lora_weight is not None else args.model_name_or_path
+                f.write( line + '\n')
+
+        task_names = []
+        scores = []
+        for task in ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']:
+            task_names.append(task)
+            if task in results:
+                scores.append("%.2f" % (results[task]['devacc']))    
+            else:
+                scores.append("0.00")
+        task_names.append("Avg.")
+        scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
+        print_table(task_names, scores)
+
+
+    elif args.mode == 'test' or args.mode == 'fasttest':
+        print("------ %s ------" % (args.mode))
+
+        task_names = []
+        scores = []
+        for task in ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']:
+            task_names.append(task)
+            if task in results:
+                if task in ['STS12', 'STS13', 'STS14', 'STS15', 'STS16']:
+                    scores.append("%.2f" % (results[task]['all']['spearman']['all'] * 100))
+                else:
+                    scores.append("%.2f" % (results[task]['test']['spearman'].correlation * 100))
+            else:
+                scores.append("0.00")
+        task_names.append("Avg.")
+        scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
+        print_table(task_names, scores)
+        #
+        # write results and template to file
+        if args.prompt is not None and args.task_set != 'transfer':
+            with open('./sts-org-results', 'a') as f:
+                bits = f'{args.load_kbit}bit'
+                model_name = args.model_name_or_path.split('/')[-1] + f'({bits})'
+                f.write(args.prompt.replace(' ', '_') + ' ' + model_name + ' ' + ' '.join([str(s) for s in scores]) + '\n')
+
+        task_names = []
+        scores = []
+        for task in ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']:
+            task_names.append(task)
+            if task in results:
+                scores.append("%.2f" % (results[task]['acc']))
+            else:
+                scores.append("0.00")
+        task_names.append("Avg.")
+        scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
+        print_table(task_names, scores)
+
+
+if __name__ == "__main__":
+    main()
