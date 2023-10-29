2d81cd0636e71ed36fe54547bb4d6110bd035413
Sean Lee <xmlee97@gmail.com>
2023-10-29 09:12:15 +0800

    download sohu test set

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,1 +1,7 @@
+echo "download AllNLI"
 wget https://sbert.net/datasets/AllNLI.tsv.gz
+
+echo "download sohu-dd"
+wget https://raw.githubusercontent.com/shibing624/text2vec/master/examples/data/SOHU/dd-test.jsonl -O ./sohu-dd.jsonl
+echo "download sohu-dc"
+wget https://raw.githubusercontent.com/shibing624/text2vec/master/examples/data/SOHU/dc-test.jsonl -O ./sohu-dc.jsonl
