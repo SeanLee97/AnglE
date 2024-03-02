2f0303f94869cab26bdb96e29ce1bb21a7ad1251
Sean Lee <xmlee97@gmail.com>
2024-03-02 14:21:54 +0800

    add test cases for 2dmse

--- tests/test_loadding.py
+++ tests/test_loadding.py
@@ -15,3 +15,14 @@ def test_loadding():
     assert isinstance(vecs, np.ndarray)
     vecs = angle.encode([{'text': 'hello world', 'text': 'hi there👋'}])
     assert isinstance(vecs, np.ndarray)
+
+
+def test_2dmse_loadding():
+    import numpy as np
+    from angle_emb import AnglE
+
+    angle = AnglE.from_pretrained('WhereIsAI/UAE-Large-V1')
+    vecs = angle.encode('hello world', layer_index=20)
+    assert isinstance(vecs, np.ndarray)
+    vecs = angle.encode(['hello world', 'hi there👋'], layer_index=20, embedding_size=512)
+    assert isinstance(vecs, np.ndarray)
