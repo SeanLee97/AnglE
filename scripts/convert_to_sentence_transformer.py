dbb819632c2f037dee3e8b4b6523ee5c0a61dfaa
Sean Lee <xmlee97@gmail.com>
2024-05-21 16:21:04 +0800

    add script: convert angle to sentence-transformers

--- scripts/convert_to_sentence_transformer.py
+++ scripts/convert_to_sentence_transformer.py
@@ -17,6 +17,7 @@ parser.add_argument('--pooling_strategy', type=str, required=True,
 parser.add_argument('--max_length', type=int, default=512,
                     help='Specify max length')
 parser.add_argument('--push_to_hub', type=int, default=0, choices=[0, 1], help='Specify push_to_hub, default 0')
+parser.add_argument('--half', type=int, default=0, choices=[0, 1], help='Specify half precision, default 0')
 parser.add_argument('--hub_private_repo', type=int, default=1, choices=[0, 1],
                     help='Specify hub_private_repo, default 1')
 parser.add_argument('--hub_model_id', type=str, default=None,
@@ -27,6 +28,8 @@ args = parser.parse_args()
 word_embedding_model = models.Transformer(args.model_name_or_path, max_seq_length=args.max_length)
 pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(), pooling_mode=args.pooling_strategy)
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+if args.half:
+    model = model.half()
 
 if args.push_to_hub:
-    model.push_to_hub(args.hub_model_id, private=args.hub_private_repo, exist_ok=True)
+    model.push_to_hub(args.hub_model_id, private=args.hub_private_repo == 1, exist_ok=True)
