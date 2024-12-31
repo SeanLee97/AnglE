5f56d5e50cbed44ef1525a28dd45c5d81d4db8a0
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2024-12-31 14:51:08 +0800

    support load from disk

--- angle_emb/angle_trainer.py
+++ angle_emb/angle_trainer.py
@@ -6,7 +6,7 @@ import random
 
 import numpy as np
 import torch
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 
 from angle_emb import AnglE, AngleDataTokenizer
 from angle_emb.utils import logger
@@ -208,10 +208,13 @@ def main():
                   load_mlm_model=args.load_mlm_model)
 
     if os.path.exists(args.train_name_or_path):
-        ds = load_dataset('json',
-                          data_files=[args.train_name_or_path],
-                          num_proc=args.workers,
-                          streaming=args.streaming)
+        if os.path.isdir(args.train_name_or_path):
+            ds = load_from_disk(args.train_name_or_path, num_proc=args.workers)
+        else:
+            ds = load_dataset('json',
+                              data_files=[args.train_name_or_path],
+                              num_proc=args.workers,
+                              streaming=args.streaming)
     else:
         ds = load_dataset(args.train_name_or_path,
                           args.train_subset_name,
@@ -238,7 +241,10 @@ def main():
     if valid_ds is None and args.valid_name_or_path is not None:
         logger.info('Validation detected, processing validation...')
         if os.path.exists(args.valid_name_or_path):
-            valid_ds = load_dataset('json', data_files=[args.valid_name_or_path], num_proc=args.workers)
+            if os.path.isdir(args.valid_name_or_path):
+                valid_ds = load_from_disk(args.valid_name_or_path, num_proc=args.workers)
+            else:
+                valid_ds = load_dataset('json', data_files=[args.valid_name_or_path], num_proc=args.workers)
         else:
             if args.valid_subset_name is not None:
                 valid_ds = load_dataset(args.valid_name_or_path, args.valid_subset_name, num_proc=args.workers)
@@ -254,8 +260,11 @@ def main():
     if valid_ds_for_callback is None and args.valid_name_or_path_for_callback is not None:
         logger.info('Validation for callback detected, processing validation...')
         if os.path.exists(args.valid_name_or_path_for_callback):
-            valid_ds_for_callback = load_dataset(
-                'json', data_files=[args.valid_name_or_path_for_callback], num_proc=args.workers)
+            if os.path.isdir(args.valid_name_or_path_for_callback):
+                valid_ds_for_callback = load_from_disk(args.valid_name_or_path_for_callback, num_proc=args.workers)
+            else:
+                valid_ds_for_callback = load_dataset(
+                    'json', data_files=[args.valid_name_or_path_for_callback], num_proc=args.workers)
         else:
             if args.valid_subset_name_for_callback is not None:
                 valid_ds_for_callback = load_dataset(
